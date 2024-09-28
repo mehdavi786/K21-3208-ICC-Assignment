91e2db24dce1744064283c6a3a1d058290045444
hasanmehdavi <hasan.mehdavi@talverse.com>
2024-09-28 21:41:42 +0500

    Initial push

---
+++ 21K-3208/Pages/Models.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _21K_3208.Pages
+{
+    public class ModelsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
